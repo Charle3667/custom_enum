1b5b6053e64add58a2c341b6dc4644f75ad6fada
Charlie Fordon <fordoncf@gmail.com>
2022-02-16 16:51:12 -0500

    some problems solved

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -6,11 +6,75 @@ module Enumerable
       yield a, b
     end
   end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    for i in 0..self.size-1 do
+      yield self[i], i
+    end
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    selected_array = []
+    selected_hash = {}
+    if self.is_a?(Array)
+      for v in self
+        selected_array.push(v) if yield v
+      end
+      selected_array
+    else
+      for k, v in self
+        selected_hash[k] = v if yield k, v
+      end
+      selected_hash
+    end
+  end
+
+  def my_all?(x = nil)
+
+    bool = true
+    for v in self
+      if x != nil
+        bool = false unless x === v
+      elsif block_given?
+        bool = false unless yield v
+      else
+        return to_enum(:my_select)
+      end
+    end
+    bool
+  end
+
+  def my_none?(x = nil)
+    bool = true
+    for v in self
+      if x != nil
+        bool = false if x === v
+      elsif block_given?
+        bool = false if yield v
+      else
+        bool = false if v
+      end
+    end
+    bool
+  end
 end
 
-puts "my_each vs each"
-numbers = [1, 2, 3, 4, 5]
-p numbers.my_each {|item| p item}
-p numbers.each {|item| p item}
-p numbers.my_each
-p numbers.each
+puts "my_none? vs none?"
+numbers = [1,2,3,4,5]
+wrong_numbers = [1,3,5,7,9]
+p numbers.my_none? {|value| value %2 == 0}
+p numbers.none? {|value| value %2 == 0}
+p wrong_numbers.my_none? {|value| value %2 == 0}
+p wrong_numbers.none? {|value| value %2 == 0}
+p [].my_none?
+p [].none?
+p [nil, true].my_none?
+p [nil, true].none?
+p [].my_none?(Float)
+p [].none?(Float)
+puts "\n\n"
+
